b238373ecc19f5ab46eeaaf12806d506704dc386
creatong <jgh3482@gmail.com>
2021-07-20 22:21:35 +0900

    project

---
+++ src/main/java/com/gb/oembed/OEmbedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gb.oembed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OEmbedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OEmbedProjectApplication.class, args);
+	}
+
+}
